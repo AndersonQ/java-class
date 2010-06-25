3d2c34f6f64f3487f330fa00d884a2a13d9dac81
Anderson Queiroz <anderson.f.queiroz@gmail.com>
2010-06-25 19:00:17 -0300

    Classe Tabuleiro criada

---
+++ 3-Xadrez/Tabuleiro.java
@@ -0,0 +1,36 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  Tabuleiro.java
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  25-06-2010 18:54:18
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Anderson de França Queiroz (A.F.Q.), anderson.f.queiroz(.AT,)gmail dot com
+ *        Company:  Universidade Federal do ABC - UFABC
+ *
+ * =====================================================================================
+ */
+
+public class Tabuleiro
+{
+	//protected ArrayList<Jogadas> jogadas;
+	protected Pecas [][] board;
+	protected ArryList<Pecas> capturadas;
+	String NextPlay;
+
+	public Tabuleiro()
+	{
+		//jogadas = new ArrayList<Jogadas>;
+		board = new Pecas[8][8];
+		capturadas = new ArrayList<Pecas>;
+		NextPlay = "branca";
+	}
+}
+
+
+	
