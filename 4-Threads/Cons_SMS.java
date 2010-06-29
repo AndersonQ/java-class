1914a9f3cec79638009b887a9c4732c69ec06122
Anderson Queiroz <anderson.f.queiroz@gmail.com>
2010-06-29 17:46:55 -0300

    Classe Cons_SMS.java criada

---
+++ 4-Threads/Cons_SMS.java
@@ -0,0 +1,45 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  Cons_SMS.java
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  29-06-2010 17:35:52
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Anderson de França Queiroz (A.F.Q.), anderson.f.queiroz(.AT,)gmail dot com
+ *        Company:  Universidade Federal do ABC - UFABC
+ *
+ * =====================================================================================
+ */
+
+public calss Cons_SMS implements Runnable
+{
+	Fila<Mensagem> f;
+
+	public Cons_SMS(Fila f)
+	{
+		this.f = f;
+	}
+
+	public run()
+	{
+		Mensagem m;
+		int tempo = 200 + (int) (random() * 600);
+
+		try
+		{
+			m = f.retira();
+		}
+		catch (InterruptedException e)
+		{
+			System.out.println(e);
+		}
+
+		System.out.println(m + "Enviada para o celular " + m.get_Dest_cel());
+	}
+}
+
