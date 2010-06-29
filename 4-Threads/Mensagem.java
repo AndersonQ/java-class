1e700997d742eea43530d7a6948c30009b7cabfd
Anderson Queiroz <anderson.f.queiroz@gmail.com>
2010-06-29 16:36:58 -0300

    Classe Mensagem cirada

---
+++ 4-Threads/Mensagem.java
@@ -0,0 +1,51 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  Mensagem.java
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  29-06-2010 16:25:56
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Anderson de França Queiroz (A.F.Q.), anderson.f.queiroz(.AT,)gmail dot com
+ *        Company:  Universidade Federal do ABC - UFABC
+ *
+ * =====================================================================================
+ */
+
+import static java.lang.Math;
+
+public class Mensagem
+{
+	private static int IDcont = 0;
+	private int ID;
+	private int Orig_Cel;
+	private int Dest_Cel;
+	private String msg;
+
+	public Mensagem(int Orig_Cel, int Dest_Cel, String msg)
+	{
+		this.ID = IDcont;
+		this.Orig_Cel = Orig_Cel;
+		this.Dest_Cel = Dest_Cel;
+		this.msg = msg;
+		IDcont++;
+	}
+
+	public Mensagem(String msg)
+	{
+		this.ID = IDcont;
+		this.Orig_Cel = random(); *100000000;
+		this.Dest_Cel = random(); *100000000;
+		this.msg = "Mensagem aleatória " + ID;
+		IDcont++;
+	}
+
+	public get_msg()
+	{
+		return msg;
+	}
+}
