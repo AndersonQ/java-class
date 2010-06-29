cbab3d82aeef85b9d88ac4d49c31c1f62c026520
Anderson Queiroz <anderson.f.queiroz@gmail.com>
2010-06-29 18:03:41 -0300

    Classe CentralSMS criada

---
+++ 4-Threads/CentralSMS.java
@@ -0,0 +1,44 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  CentralSMS.java
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  29-06-2010 17:52:54
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Anderson de França Queiroz (A.F.Q.), anderson.f.queiroz(.AT,)gmail dot com
+ *        Company:  Universidade Federal do ABC - UFABC
+ *
+ * =====================================================================================
+ */
+
+public class CentralSMS
+{
+	FilaPC f;
+
+	public CentralSMS()
+	{
+		f = new FilaPC(1000);
+	}
+
+	public void start()
+	{
+		Cons_SMS c = new Cons_SMS(f);
+
+		Prod_SMS p1 = new Prod_SMS(f);
+		Prod_SMS p2 = new Prod_SMS(f);
+		Prod_SMS p3 = new Prod_SMS(f);
+
+		Thread P1 = new Thread(p1);
+		Thread P2 = new Thread(p2);
+		Thread P3 = new Thread(p3);
+
+		P1.start();
+		P2.start();
+		P3.start();
+	}
+}
