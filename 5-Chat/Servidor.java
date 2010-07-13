c535b6382b15c6c490d0258aa3c863ba9b6e235d
Anderson Queiroz <anderson.f.queiroz@gmail.com>
2010-07-13 17:57:28 -0300

    Servidor implemetado

--- 5-Chat/Servidor.java
+++ 5-Chat/Servidor.java
@@ -16,4 +16,48 @@
  * =====================================================================================
  */
 
+import java.net.*;
 
+public class Servidor
+{
+	Fila<Mensagem> fm;
+	ServerSocket server;
+
+	public Servidor(int num)
+	{
+		try
+		{
+			server = new ServerSocket(num);
+		}
+		catch(Exception e)
+		{
+			System.out.println(e);
+		}
+
+		fm = new Fila<Mensagem>(1000);
+	}
+
+	public void run()
+	{
+		Consumidora c1 = new Consumidora(fm);
+		Thread t1 = new Thread(c1);
+
+		t1.run();
+
+		while(true)
+		{
+			ConexaoCliente cc = null;
+			try
+			{
+				cc = new ConexaoCliente(server.accept(), fm);
+			}
+			catch(Exception e)
+			{
+				System.out.println(e);
+			}
+
+			Thread t2 = new Thread(cc);
+			t2.run();
+		}
+	}
+}
