584d21c85e6ca9e629db8208d0728efe559879bf
Anderson Queiroz <anderson.f.queiroz@gmail.com>
2010-07-13 17:17:59 -0300

    Makefike adicionado e Classe Servidor criada

---
+++ 5-Chat/Servidor.java
@@ -0,0 +1,19 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  Servidor.java
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  13-07-2010 17:15:56
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Anderson de França Queiroz (A.F.Q.), anderson.f.queiroz(.AT,)gmail dot com
+ *        Company:  Universidade Federal do ABC - UFABC
+ *
+ * =====================================================================================
+ */
+
+
